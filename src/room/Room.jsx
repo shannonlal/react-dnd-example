fe716575640ebcde97ef5cf3167708e850179c22
Shannon Lal <shannonlal@hotmail.com>
2019-10-27 12:23:52 -0400

    Adding Room Elements

--- src/room/Room.jsx
+++ src/room/Room.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDrop } from 'react-dnd'
 import ItemTypes from './ItemTypes'
-
-import RoomItem from './room/RoomItem'
+import RoomItem from './RoomItem'
+import update from 'immutability-helper'
 const styles = {
   width: 300,
   height: 300,
   border: '1px solid black',
   position: 'relative',
 }
-const Room = ({ hideSourceOnDrag, rooms, moveRoom }) => {
-
+const Room = ({ hideSourceOnDrag }) => {
+  const [roomsItems, setRoomsItems] = useState({
+    a: { top: 20, left: 80, title: 'Drag me around' },
+    b: { top: 180, left: 20, title: 'Drag me too' },
+  })
   const [, drop] = useDrop({
     accept: ItemTypes.BOX,
     drop(item, monitor) {
       const delta = monitor.getDifferenceFromInitialOffset()
       const left = Math.round(item.left + delta.x)
       const top = Math.round(item.top + delta.y)
-      moveRoom(item.id, left, top)
+      moveRoomItem(item.id, left, top)
       return undefined
     },
   })
-
+  const moveRoomItem = (id, left, top) => {
+    setRoomsItems(
+      update(roomItems, {
+        [id]: {
+          $merge: { left, top },
+        },
+      }),
+    )
+  }
   return (
     <div ref={drop} style={styles}>
-      {rooms && rooms.map((room,key) => {
-        const { left, top, title } = room;
+      {Object.keys(roomsItems).map(key => {
+        const { left, top, title } = roomItems[key]
         return (
           <RoomItem
             key={key}
@@ -39,7 +50,6 @@ const Room = ({ hideSourceOnDrag, rooms, moveRoom }) => {
         )
       })}
     </div>
-
   )
 }
 export default Room;
