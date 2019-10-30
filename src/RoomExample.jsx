20f03dc20acc00ba97b9b4c4d2bfc45598d7f2cf
Shannon Lal <shannonlal@hotmail.com>
2019-10-29 20:50:35 -0400

    Check in fix for useState

--- src/RoomExample.jsx
+++ src/RoomExample.jsx
@@ -2,15 +2,18 @@ import React, { useState, useCallback } from 'react';
 import Room from './room/Room';
 import update from 'immutability-helper'
 export default function DragAroundNaive() {
-  //const [hideSourceOnDrag, setHideSourceOnDrag] = useState(true)
+
+  const [hideSourceOnDrag, setHideSourceOnDrag] = useState(true)
   const [roomDetails, setRoomDetails] = useState({
-    hideSourceOnDrag: true,
     elements: [
       { top: 20, left: 80, title: 'Drag me around' },
       { top: 180, left: 20, title: 'Drag me too' }
     ]
   })
-  const toggle = useCallback(() => setRoomDetails({hideSourceOnDrag:!roomDetails.hideSourceOnDrag}));
+  const toggle = useCallback(() => {
+    console.log( 'Toggling field');
+    setHideSourceOnDrag(!hideSourceOnDrag)
+  });
 
   const moveRoomItem = (id, left, top) => {
     setRoomDetails(
@@ -37,13 +40,13 @@ export default function DragAroundNaive() {
   console.log( 'Room Details', roomDetails);
   return (
     <div>
-      <Room hideSourceOnDrag={roomDetails.hideSourceOnDrag} roomItems={roomDetails} moveRoomItem={moveRoomItem} addRoomItem={addRoomItem} />
+      <Room hideSourceOnDrag={hideSourceOnDrag} roomItems={roomDetails} moveRoomItem={moveRoomItem} addRoomItem={addRoomItem} />
       <p>
         <label htmlFor="hideSourceOnDrag">
           <input
             id="hideSourceOnDrag"
             type="checkbox"
-            checked={roomDetails.hideSourceOnDrag}
+            checked={hideSourceOnDrag}
             onChange={toggle}
           />
           <small>Hide the source item while dragging</small>
